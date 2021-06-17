32283e1484e1c12b4b5b4024b5c04b0c876c33a4
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2021-06-17 14:22:22 -0700

    Add actions with basic setup to inject code snippets in the header and after the opening body tag.

--- plugins/yaydoo-gtm/yaydoo-gtm.php
+++ plugins/yaydoo-gtm/yaydoo-gtm.php
@@ -1,12 +1,12 @@
 <?php
 /*
  * Plugin Name: Yaydoo GTM
- * Plugin URI:  https://yaydoo.com/
+ * Plugin URI:	https://yaydoo.com/
  * Description: Add Google Tag Manager.
- * Version:     1.0
- * Author:      Sandra Vargas
- * Author URI:  https://yaydoo.com/
- * License:     GPL-2.0+
+ * Version:		 1.0
+ * Author:			Sandra Vargas
+ * Author URI:	https://yaydoo.com/
+ * License:		 GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: yaydoo-gtm
  * Domain Path: /languages
@@ -17,10 +17,39 @@ defined( 'ABSPATH' ) || exit;
 define( 'YAYDOO_GTM_PLUGIN_FILE', __FILE__ );
 
 /**
+ * Insert Google Tag Manager's code snippet in the header.
+ */
+add_action( 'wp_head', 'yaydoo_insert_code_header' );
+
+function yaydoo_insert_code_header() {
+	// Insert functionality to add Google Tag Manager ?>
+	<style>
+		body {
+			background-color : #f1f1f1;
+		}
+	</style>
+	<?php
+}
+
+/**
+ * Insert Google Tag Manager's code snippet when no script is true.
+ */
+add_action( 'wp_body_open', 'yaydoo_insert_code_no_script' );
+add_action( 'wp_footer', 'yaydoo_insert_code_no_script' );
+
+function yaydoo_insert_code_no_script() {
+	if ( doing_action( 'wp_body_open' ) ) {
+		remove_action( 'wp_footer', 'yaydoo_insert_code_no_script' );
+	}
+
+	echo 'This is inserted in case of no script.';
+}
+
+/**
  * Activate the plugin.
  */
 function yaydoo_gtm_activate() {
-	// Insert functionality to add GTM
+	// Insert functionality for when you activate the plugin
 }
 register_activation_hook( YAYDOO_GTM_PLUGIN_FILE, 'yaydoo_gtm_activate' );
 
