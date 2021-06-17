41572bf11bda4e2c49dfd2f695237c94a17f0f89
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2021-06-16 20:31:10 -0700

    Add child theme with initial setup.

---
+++ themes/twentytwentyone-yaydoo/functions.php
@@ -0,0 +1,11 @@
+<?php
+// Enqueue parent styles
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+	wp_enqueue_style(
+		'child-style',
+		get_stylesheet_uri(),
+		array( 'twentytwentyone' ),
+		wp_get_theme()->get( 'Version' ) // this only works if you have Version in the style header
+	);
+}
